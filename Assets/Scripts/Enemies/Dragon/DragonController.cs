90a206a4471646af73ba618652ab646ae1fbc675
psyjl16 <psyjl16@nottingham.ac.uk>
2023-04-11 14:26:12 +0100

    Added dragon attack animation on proximity

--- Assets/Scripts/Enemies/Dragon/DragonController.cs
+++ Assets/Scripts/Enemies/Dragon/DragonController.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class DragonController : MonoBehaviour
 {
+
+    // Animation variables
+    bool attacking;
+    bool dead; 
+    int attackingKey = Animator.StringToHash("attacking");
+    int deadKey = Animator.StringToHash("dead");
+
+    Animator anim;
+
+    // Behaviour
+    [Header("Behaviour")]
+    [SerializeField] float attackDelay;
+    [SerializeField] float attackDuration;
+
+    bool inRange = false;
+    float previousAttack;
+
+    GameObject target;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        previousAttack = Time.time - attackDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            if (!(target = GameObject.Find("Player(Clone)")))
+                return;
+
+        if (inRange && Time.time > previousAttack + attackDelay)
+        {
+            StartCoroutine(BreathFire());
+            previousAttack = Time.time;
+        }
+        anim.SetBool(attackingKey, attacking);
+        anim.SetBool(deadKey, dead);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            inRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            inRange = false;
+    }
+
+    IEnumerator BreathFire()
+    {
+        attacking = true;
+        yield return new WaitForSeconds(attackDuration);
+        attacking = false;
     }
 }
